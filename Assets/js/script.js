4ff55ecb7c0d09d1c7172c1548bfb3842f32fc0b
Chris <157438747+ChrisVulpine@users.noreply.github.com>
2024-03-08 16:34:02 -0500

    finished function that stores user input in local storage and displays it once page is refreshed.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,4 @@
-// ✅Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
@@ -25,27 +25,46 @@
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
-  // TODO: Add code to get any user input that was saved in localStorage and set
+  // ✅TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // ✅TODO: Add code to display the current date in the header of the page.
 //});
 
-// ------------Currently Building: when save button clicked, log value of textarea-----------------------------------//
+// ------------TEMPLATE for local storage hour 9 - use this for all other hours. This stores and pulls the inputs in-----------------------------------//
 
-// ISSUES //  does not save to local storage, how can I test this? Would an interrupt help? 
+var storedData = JSON.parse(localStorage.getItem('commentKey'));
+
+var display = document.getElementById('potato');
+
+if (storedData) {
+  display.innerHTML = storedData; 
+}
+
+else {
+  display.innerHTML = '';
+}
 
 $('#hour-9').find('button').click(function() {
   $(this).find('textarea');
   let comment = $.trim($("#potato").val());
   console.log(comment);
+  
 
-  localStorage.setItem('comment','KEY');
-  let event = localStorage.getItem('comment');
-  console.log(event);
+  localStorage.setItem('commentKey', JSON.stringify(comment));
+  let userEntry = localStorage.getItem('commentKey');
+  var hourNine = JSON.parse(localStorage.getItem('commentKey'));
+  console.log(userEntry);
+  console.log(hourNine);
+
+  
 });
+
 // -----------------------------------------------------------------//
+
+
+
 
 
 // Current Date formatted with AdvancedFormat extension //
@@ -56,5 +75,3 @@ var currentDayEl = dayjs().format('dddd MMMM Do,YYYY');
 $('#currentDay').text(currentDayEl);
 
 // -----------------------------------------------------------------//
-
-
